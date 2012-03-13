3b6376f38cb14460bddd26eb7bff93f6a40151cb
Ken Erickson <bookworm.productions@gmail.com>
2012-03-12 18:28:45 -0700

    remove extra junk

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -3,27 +3,7 @@ require 'hotcocoa'
 require 'lib/rpreferences/rpreferences'    
 framework 'WebKit'     
 
-# Load Menus     
-Dir.glob("lib/menus/*.rb").each do |file|
-  require file
-end      
-
-# Load Models
-Dir.glob("lib/models/*.rb").each do |file|
-  require file
-end     
-
 # Load Controllers
 Dir.glob("lib/controllers/*.rb").each do |file|
   require file
 end      
-
-# Load Views       
-Dir.glob("lib/views/*.rb").each do |file|
-  require file
-end     
-
-# Load Window Controllers  
-Dir.glob("lib/windows/*.rb").each do |file|
-  require file
-end
--- lib/views/PreferencesView.rb
+++
@@ -1,7 +0,0 @@
-class PreferencesView    
-  def initialize()           
-    views = []
-    views << {:view => AccountsView.create(), :label => 'Accounts'}
-    PreferencesWinController.create(@win, views)
-  end 
-end
